892f19e07dce460f7ec7026b109fe17fe19a31b7
Anahita Chhetri <chhetri.anahita@gmail.com>
2026-01-08 03:19:10 +1300

    biderectional_arrows program

--- bidirectional_lines.py
+++ bidirectional_lines.py
@@ -2,21 +2,23 @@ import cv2
 import numpy as np
 
 image=np.zeros((500, 800, 3), dtype="uint8")
-text="Bidirectional Arrows"
+text="Image Width: 800 pixels"
 font=cv2.FONT_HERSHEY_SIMPLEX
 scale=1
 thickness=1
 text_width, text_height=cv2.getTextSize(text,font,scale,thickness)
-
 image_width=image.shape[1]
-text_x=(image_width-text_height)//2
-text_y=text_height+20
+text_x=(image_width-text_height)//3
+text_y=text_height+230
 cv2.putText(image, text, (text_x,text_y), font, scale, (0,255,255), 2, cv2.LINE_AA)
+
+
 
 
 cv2.arrowedLine(image, (800,450),(100,450), (255,255,0), 3)
 cv2.arrowedLine(image, (100,50), (800, 50), (255, 255,0), 3)
 
-cv2.imshow("Arrows:", image)
+cv2.imshow("Bidirectional Arrows", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+cv2.imwrite("biderectional_arrows.png", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
